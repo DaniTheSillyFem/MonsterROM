82135285dccb20444a1aa82a1ff6dccefae08ba6
ditternation <ditternation@outlook.com>
2026-05-17 00:20:59 +0200

    vintf: allow newer platform SDK boots

--- unica/patches/vintf/customize.sh
+++ unica/patches/vintf/customize.sh
@@ -1,5 +1,59 @@
 _LOG() { if $DEBUG; then LOGW "$1"; else ABORT "$1"; fi }
 
+ENSURE_FRAMEWORK_SYSTEM_SDK()
+{
+    local MANIFEST="$WORK_DIR/system/system/etc/vintf/manifest.xml"
+    local CONFIG_SDK="$SOURCE_PLATFORM_SDK_VERSION"
+    local PROP_SDK
+    local SDK
+    local TMP
+
+    if [ ! -f "$MANIFEST" ]; then
+        return 0
+    fi
+
+    PROP_SDK="$(GET_PROP "system" "ro.build.version.sdk")"
+    SDK="$CONFIG_SDK"
+    if [[ "$PROP_SDK" =~ ^[0-9]+$ ]] && {
+        [ ! "$SDK" ] || [ "$PROP_SDK" -gt "$SDK" ]
+    }; then
+        SDK="$PROP_SDK"
+    fi
+
+    if ! [[ "$SDK" =~ ^[0-9]+$ ]]; then
+        return 0
+    fi
+
+    # libvintf rejects boot when ro.build.version.sdk is newer than framework manifest system-sdk.
+    if sed -n '/<system-sdk>/,/<\/system-sdk>/p' "$MANIFEST" | grep -q "<version>$SDK</version>"; then
+        return 0
+    fi
+
+    LOG "- Adding system SDK $SDK to ${MANIFEST//$WORK_DIR/}"
+    TMP="$(mktemp)"
+    if grep -q "<system-sdk>" "$MANIFEST"; then
+        awk -v sdk="$SDK" '
+            /<\/system-sdk>/ && !done {
+                print "        <version>" sdk "</version>"
+                done = 1
+            }
+            { print }
+        ' "$MANIFEST" > "$TMP"
+    else
+        awk -v sdk="$SDK" '
+            /<\/manifest>/ && !done {
+                print "    <system-sdk>"
+                print "        <version>" sdk "</version>"
+                print "    </system-sdk>"
+                done = 1
+            }
+            { print }
+        ' "$MANIFEST" > "$TMP"
+    fi
+
+    EVAL "mv -f \"$TMP\" \"$MANIFEST\""
+}
+
 if [ -f "$SRC_DIR/target/$TARGET_CODENAME/vintf/compatibility_matrix.device.xml" ]; then
     LOG "- Adding /system/system/etc/vintf/compatibility_matrix.device.xml"
     EVAL "cp -a \"$SRC_DIR/target/$TARGET_CODENAME/vintf/compatibility_matrix.device.xml\" \"$WORK_DIR/system/system/etc/vintf/compatibility_matrix.device.xml\""
@@ -18,4 +72,6 @@ else
     LOG "- Target has no Vendor interface objects blobs change needed . Ignoring."
 fi
 
-unset -f _LOG
+ENSURE_FRAMEWORK_SYSTEM_SDK
+
+unset -f _LOG ENSURE_FRAMEWORK_SYSTEM_SDK
