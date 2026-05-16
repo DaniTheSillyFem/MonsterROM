7d3ac67c92c8fff2f213f6e2fa6ec85c32323f6c
ditternation <ditternation@outlook.com>
2026-05-16 23:06:58 +0200

    selinux: clean incompatible vendor mappings

--- unica/patches/selinux/customize.sh
+++ unica/patches/selinux/customize.sh
@@ -9,6 +9,12 @@
 ENTRIES+="
 heatmap_default
 heatmap_default_exec
+sec_diag
+sec_diag_exec
+vendor_display_notch_prop
+vendor_hal_systemhelper_hwservice
+vendor_sys_qti_display
+vendor_systemhelper_app
 "
 
 DUPLICATES+="
@@ -57,29 +63,78 @@ GET_SYSTEM_EXT()
 
 CIL_NAME="$(head -n 1 "$WORK_DIR/vendor/etc/selinux/plat_sepolicy_vers.txt")"
 
-VENDOR_API_LIST="$(find "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping" -type f -printf "%f\n" | \
-                    sed '/.compat./d' | sed 's/.cil//' | sed 's/\./_/' | sort)"
+SELINUX_DIRS="
+$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux
+$WORK_DIR/product/etc/selinux
+"
+
+VENDOR_API_LIST="$(for d in $SELINUX_DIRS; do
+                        [ -d "$d/mapping" ] || continue
+                        find "$d/mapping" -type f -printf "%f\n"
+                    done | sed '/.compat./d' | sed 's/.cil//' | sed 's/\./_/' | sort -u)"
 # ]
 
 for e in $ENTRIES; do
-    if grep -q -F "($e)" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil" || \
-         grep -q -F "${e}_${CIL_NAME//./_}" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"; then
-        # the problematic entry is currently present in system_ext, check if we need to remove it
-        if ! grep -q -F "(type $e)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
-            # the problematic entry is not supported by the target device
-            LOG "- \"$e\" SELinux entry not supported. Removing"
-            sed -i "/($e)/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
+    # the problematic entry is not supported by the target device
+    if ! grep -q -F "(type $e)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
+        for d in $SELINUX_DIRS; do
+            MAPPING_FILE="$d/mapping/$CIL_NAME.cil"
+            [ -f "$MAPPING_FILE" ] || continue
+
+            if ! grep -q -F "($e)" "$MAPPING_FILE" && \
+                 ! grep -q -F "${e}_" "$MAPPING_FILE"; then
+                continue
+            fi
+
+            # the problematic entry is currently present in this partition mapping
+            LOG "- \"$e\" SELinux entry not supported in ${d#$WORK_DIR/}. Removing"
+            sed -i "/($e)/d" "$MAPPING_FILE"
             for a in $VENDOR_API_LIST; do
-                sed -i "/${e}_${a}/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
+                sed -i "/${e}_${a}/d" "$MAPPING_FILE"
             done
-            if grep -q "genfscon.*$e" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"; then
-                sed -i "/genfscon.*$e/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"
+        done
+
+        for f in \
+            "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil" \
+            "$WORK_DIR/product/etc/selinux/product_sepolicy.cil" \
+            "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"; do
+            [ -f "$f" ] || continue
+            if grep -q "genfscon.*$e" "$f"; then
+                sed -i "/genfscon.*$e/d" "$f"
             fi
-            if grep -q "genfscon.*$e" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"; then
-                sed -i "/genfscon.*$e/d" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
-            fi
-        fi
+        done
     fi
+done
+
+_CLEAN_UNDECLARED_MAPPING_ATTRS()
+{
+    local MAPPING_FILE="$1"
+    local ATTR_LIST
+    local ATTR
+    local ATTR_REGEX
+
+    [ -f "$MAPPING_FILE" ] || return 0
+
+    ATTR_LIST="$(mktemp)"
+    grep -o -E '[A-Za-z0-9_.+/@:-]+_([0-9]+_0|[0-9]{6})' "$MAPPING_FILE" | sort -u > "$ATTR_LIST"
+
+    while read -r ATTR; do
+        [ "$ATTR" ] || continue
+        if grep -q -F "(typeattribute $ATTR)" "$MAPPING_FILE" || \
+             grep -q -F "(typeattribute $ATTR)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
+            continue
+        fi
+
+        ATTR_REGEX="$(sed 's/[][\/.^$*+?{}|()]/\\&/g' <<< "$ATTR")"
+        LOG "- \"$ATTR\" SELinux mapping attribute not declared by target vendor. Removing"
+        sed -i "/$ATTR_REGEX/d" "$MAPPING_FILE"
+    done < "$ATTR_LIST"
+
+    rm -f "$ATTR_LIST"
+}
+
+for d in $SELINUX_DIRS; do
+    _CLEAN_UNDECLARED_MAPPING_ATTRS "$d/mapping/$CIL_NAME.cil"
 done
 
 for e in $DUPLICATES; do
@@ -93,5 +148,82 @@ for e in $DUPLICATES; do
     fi
 done
 
-unset ENTRIES DUPLICATES CIL_NAME VENDOR_API_LIST
-unset -f GET_SYSTEM_EXT
+LOG_STEP_IN "- Adding missing vendor service contexts"
+_TYPE_EXISTS()
+{
+    grep -R -q -F "(type $1)" \
+        "$WORK_DIR/system/system/etc/selinux" \
+        "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux" \
+        "$WORK_DIR/vendor/etc/selinux" 2> /dev/null
+}
+
+_APPEND_CONTEXT()
+{
+    local FILE="$1"
+    local NAME="$2"
+    local TYPE="$3"
+
+    if ! _TYPE_EXISTS "$TYPE"; then
+        LOGW "SELinux type not found for $NAME: $TYPE"
+        return 0
+    fi
+
+    if ! grep -q -F "$NAME" "$FILE"; then
+        LOG "- Adding $NAME -> $TYPE"
+        printf "%-80s u:object_r:%s:s0\n" "$NAME" "$TYPE" >> "$FILE"
+    fi
+}
+
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_service_contexts" "vendor.samsung.hardware.security.vaultkeeper.ISehVaultKeeper/default" "VaultKeeper_service"
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_service_contexts" "vendor.samsung.hardware.security.hermes.ISehHermesCommand/default" "Hermes_service"
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_service_contexts" "vendor.samsung.hardware.sysinput.ISehSysInputDev/default" "SemInputDeviceManager_service"
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_service_contexts" "vendor.samsung.hardware.radio.bridge.ISehRadioBridge/slot1" "hal_radio_service"
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_service_contexts" "vendor.samsung.hardware.radio.bridge.ISehRadioBridge/slot2" "hal_radio_service"
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_service_contexts" "vendor.samsung.hardware.bluetooth.audio.ISehBluetoothAudioProviderFactory/default" "hal_audio_service"
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_service_contexts" "vendor.samsung.hardware.nfc_aidl.ISehNfc/default" "hal_nfc_service"
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_service_contexts" "android.hardware.security.keymint.IRemotelyProvisionedComponent/strongbox" "hal_remotelyprovisionedcomponent_service"
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_service_contexts" "vendor.qti.hardware.display.config.IDisplayConfig/default" "vendor_hal_displayconfig_service"
+_APPEND_CONTEXT "$WORK_DIR/vendor/etc/selinux/vendor_hwservice_contexts" "vendor.display.config::IDisplayConfig" "hal_vendor_configstore_hwservice"
+
+_APPEND_PROP_ALLOW()
+{
+    local FILE="$1"
+    local DOMAIN="$2"
+    local TYPE="$3"
+    local RULE
+
+    if ! _TYPE_EXISTS "$DOMAIN"; then
+        LOGW "SELinux domain not found for property allow: $DOMAIN"
+        return 0
+    fi
+
+    if ! _TYPE_EXISTS "$TYPE"; then
+        LOGW "SELinux property type not found for $DOMAIN: $TYPE"
+        return 0
+    fi
+
+    RULE="(allow $DOMAIN $TYPE (property_service (set)))"
+    if ! grep -q -F "$RULE" "$FILE"; then
+        LOG "- Allowing $DOMAIN to set $TYPE"
+        printf "%s\n" "$RULE" >> "$FILE"
+    fi
+
+    RULE="(allow $DOMAIN $TYPE (file (read getattr map open)))"
+    if ! grep -q -F "$RULE" "$FILE"; then
+        printf "%s\n" "$RULE" >> "$FILE"
+    fi
+}
+
+SYSTEM_EXT_SEPOLICY="$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"
+_APPEND_PROP_ALLOW "$SYSTEM_EXT_SEPOLICY" "emservice" "vendor_em_tstate_prop"
+_APPEND_PROP_ALLOW "$SYSTEM_EXT_SEPOLICY" "emservice" "em_version_prop"
+_APPEND_PROP_ALLOW "$SYSTEM_EXT_SEPOLICY" "hermesd" "vendor_securehw_prop"
+_APPEND_PROP_ALLOW "$SYSTEM_EXT_SEPOLICY" "hermesd" "vendor_securenvm_prop"
+_APPEND_PROP_ALLOW "$SYSTEM_EXT_SEPOLICY" "snap_utility" "cache_status_prop"
+
+unset SYSTEM_EXT_SEPOLICY
+unset -f _TYPE_EXISTS _APPEND_CONTEXT _APPEND_PROP_ALLOW
+LOG_STEP_OUT
+
+unset ENTRIES DUPLICATES CIL_NAME SELINUX_DIRS VENDOR_API_LIST MAPPING_FILE
+unset -f GET_SYSTEM_EXT _CLEAN_UNDECLARED_MAPPING_ATTRS
